073250c9bbff888f3d953233de961fac2ae8ce38
sirteddanson <knowledgeMED@gmail.com>
2022-04-06 19:40:13 -0400

    client-side queries/mutations

--- client/utils/mutations.js
+++ client/utils/mutations.js
@@ -1,1 +1,42 @@
 import { gql } from '@apollo/client';
+
+export const ADD_USER = gql`
+mutation addUser(
+    $username: String!
+    $email: String!
+    $password: String!
+) {
+    addUser(
+    username: $username
+    email: $email
+    password: $password
+    ) {
+        token
+    user {
+            _id
+        }
+    }
+}
+`;
+
+export const LOGIN = gql`
+mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+    token
+    user {
+    _id
+        }
+    }
+}
+`;
+
+export const ADD_MESSAGE = gql`
+mutation addMessage($messageText: String!) {
+    addMessage(messageText: $messageText) {
+    _id
+    messageText
+    createdAt
+    username
+    }
+}
+`;
--- client/utils/queries.js
+++ client/utils/queries.js
@@ -1,12 +1,51 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_MESSAGES = gql`
-  query messages($username: String) {
-    messages(username: $username) {
-      _id
-      messageText
-      createdAt
-      username
+export const QUERY_ME = gql`
+{
+    me {
+    _id
+    username
+    email
+    messages {
+        _id
+        messageText
+        createdAt
+            }
+        }
     }
-  }
+`;
+export const QUERY_USER = gql`
+    query user($username: String!) {
+    user(username: $username) {
+    _id
+    username
+    email
+    messages {
+        _id
+        messageText
+        createdAt
+            }
+        }
+    }
+`;
+export const QUERY_MESSAGE = gql`
+    query message($id: ID!) {
+    message(_id: $id) {
+    _id
+    messageText
+    createdAt
+    username
+        }
+    }
+`;
+
+export const QUERY_MESSAGES = gql`
+    query messages($username: String) {
+    messages(username: $username) {
+    _id
+    messageText
+    createdAt
+    username  
+        }
+    }
 `;
